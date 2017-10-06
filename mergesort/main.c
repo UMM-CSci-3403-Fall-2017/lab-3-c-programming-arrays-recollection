2f5a4746cbea69e509bc8401df5e3b3461622ffc
root <root@DESKTOP-AD8UU62.localdomain>
2017-10-06 12:43:14 -0500

    added code for main.c to work correctly.

---
+++ mergesort/main.c
@@ -0,0 +1,16 @@
+#include "mergesort.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int i;
+    int arr[]= {12, 13, 11, 6, 7, 5};
+    int arrSize = sizeof(arr)/sizeof(arr[0]);
+    
+    mergeSort(arrySize, arry)
+    
+    for (i=0; i< arrSize; i++){
+        printf("%d", arr[i]);
+        printf("/n");
+    }
+}
