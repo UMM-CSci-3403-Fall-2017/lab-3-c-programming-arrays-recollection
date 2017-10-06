e3dee8d2ff5880f44c0f7a567d59a831a5965bf8
root <root@DESKTOP-AD8UU62.localdomain>
2017-10-06 12:53:29 -0500

    got the mergeSort portion added, need to get the actually sorting done, ie on the merge portion of the array, maybe some for and while loops.

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -2,3 +2,18 @@
 #include <stdlib.h>
 
 
+void mergeSort(int arr[], int  x){
+    int left = 0;
+    int right = x;
+    if (left < right){
+	//Middle of the array.
+        int middle = left+(right-1)/2;
+	
+        //Splits the arry into two
+        mergeSort(arr, left+1, m);
+        mergeSort(arr, middle+1, right);
+        
+        merge(arr, left, middle, right);
+    }
+}
+
