42c9d94854e7dd78744b0fc917290a86a6bbb7fb
root <root@DESKTOP-AD8UU62.localdomain>
2017-10-07 12:20:07 -0500

    Added more for mergesort

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,19 +1,61 @@
-#include "mergesort.h"
-#include <stdlib.h>
+#include"mergesort.h"
+#include <stdio.h>
 
+void merge(int arr[], int left, int middle, int right){
+    int i, j, k;
+    int CountLeftArry = middle - left + 1;
+    int CountRightArry =  right - middle;
 
-void mergeSort(int arr[], int  x){
-    int left = 0;
-    int right = x;
-    if (left < right){
-	//Middle of the array.
-        int middle = left+(right-1)/2;
-	
-        //Splits the arry into two
-        mergeSort(arr, left+1, middle);
-        mergeSort(arr, middle+1, right);
-        
-        merge(arr, left, middle, right);
+    int TmpArrLeft[CountLeftArry], TmpArrRight[CountRightArry];
+ 
+    for (i = 0; i < CountLeftArry; i++)
+        TmpArrLeft[i] = arr[left + i];
+    for (j = 0; j < CountRightArry; j++)
+        TmpArrRight[j] = arr[middle + 1+ j];
+ 
+    i = 0, j = 0;
+    k = left;
+    while (i < CountLeftArry && j < CountRightArry){
+        if (TmpArrLeft[i] <= TmpArrRight[j]){
+            arr[k] = TmpArrLeft[i];
+            i++;
+        }
+        else{
+            arr[k] = TmpArrRight[j];
+            j++;
+        }
+        k++;
     }
+ 
+    while (i < CountLeftArry){
+        arr[k] = TmpArrLeft[i];
+        i++;
+        k++;
+    }
+ 
+    while (j < CountRightArry){
+        arr[k] = TmpArrRight[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int ArrToSort[], int left, int right){
+	if (left < right){
+	    int middle = left+(right-left)/2;
+
+        //Splits into two sizes.
+		mergeSort(ArrToSort, left, middle);
+		mergeSort(ArrToSort, middle+1, right);
+
+		merge(ArrToSort, left, middle, right);
+	}
+}
+
+void printArray(int A[], int size){
+	int i;
+	for (i=0; i < size; i++)
+		printf("%d ", A[i]);
+	printf("\n");
 }
 
