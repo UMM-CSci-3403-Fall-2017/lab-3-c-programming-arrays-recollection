0e7632ff91bf77a266fe74b939e24be85b350673
root <root@DESKTOP-AD8UU62.localdomain>
2017-10-07 11:37:33 -0500

    fixed a typo

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -10,7 +10,7 @@ void mergeSort(int arr[], int  x){
         int middle = left+(right-1)/2;
 	
         //Splits the arry into two
-        mergeSort(arr, left+1, m);
+        mergeSort(arr, left+1, middle);
         mergeSort(arr, middle+1, right);
         
         merge(arr, left, middle, right);
